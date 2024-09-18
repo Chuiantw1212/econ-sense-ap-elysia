9584fcc51477215b19d8feb41c6a1d708c8a7ca3
Chuian <chuiantw1212@gmail.com>
2024-09-18 21:15:38 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ import metaController from './adapters/blog.in/meta.ctrl'
         OPENAI_API_KEY = await googleCloud.accessSecret('OPENAI_API_KEY')
     } catch (error: any) {
         console.trace('OPENAI_API_KEY:', error.message)
-        const keyPath = path.resolve(__dirname, '../OPEN_API_KEY.json')
+        const keyPath = path.resolve(__dirname, './OPEN_API_KEY.json')
         OPENAI_API_KEY = require(keyPath);
     }
     chatGpt.initializeSync(OPENAI_API_KEY)
@@ -70,7 +70,7 @@ import metaController from './adapters/blog.in/meta.ctrl'
         FIREBASE_SERVICE_ACCOUNT_KEY_JSON = await googleCloud.accessSecret('FIREBASE_SERVICE_ACCOUNT_KEY_JSON')
     } catch (error: any) {
         console.trace('FIREBASE_SERVICE_ACCOUNT_KEY_JSON:', error.message)
-        const keyPath = path.resolve(__dirname, '../FIREBASE_SERVICE_ACCOUNT_KEY_JSON.json')
+        const keyPath = path.resolve(__dirname, './FIREBASE_SERVICE_ACCOUNT_KEY_JSON.json')
         FIREBASE_SERVICE_ACCOUNT_KEY_JSON = require(keyPath);
     }
     const firestore = await firebase.initializeSync(FIREBASE_SERVICE_ACCOUNT_KEY_JSON)
